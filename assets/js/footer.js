339639251cff5341a0e339c7f9d2bf6578a18fc3
Thu Nguyen <tqn14@tamu.edu>
2021-07-02 13:17:08 -0500

    first commit

---
+++ assets/js/footer.js
@@ -0,0 +1,37 @@
+document.write('<section id="footer"> \
+    <div class="inner">\
+    <h2 class="major">Get in touch</h2>\
+    <p>Let&#8217s connect!</p> \
+    <form method="post" action="#">\
+        <div class="fields">\
+            <div class="field">\
+                <label for="name">Name</label>\
+                <input type="text" name="name" id="name" />\
+            </div>\
+            <div class="field">\
+                <label for="email">Email</label>\
+                <input type="email" name="email" id="email" />\
+            </div>\
+            <div class="field">\
+                <label for="message">Message</label>\
+                <textarea name="message" id="message" rows="4"></textarea>\
+            </div>\
+        </div>\
+        <ul class="actions">\
+            <li><input type="submit" value="Send Message" /></li>\
+        </ul>\
+    </form>\
+    <ul class="contact">\
+        <li class="icon solid fa-phone">(971) 401-7226</li>\
+        <li class="icon solid fa-envelope"><a href="mailto:tqn1472@gmail.com">tqn1472@gmail.com</a></li>\
+        <li class="icon brands fa-twitter"><a href="https://twitter.com/tqn1472">twitter.com/tqn1472</a></li>\
+        <li class="icon brands fa-github"><a href="https://github.com/tqn14">github.com/tqn14</a></li>\
+        <li class="icon brands fa-linkedin"><a href="https://linkedin.com/in/thuquynhnguyen">linkedin.com/in/thuquynhnguyen</a></li>\
+        <li class="icon fa-file"><a href="ThuNguyen (2).pdf" target="_blank">Resume</a></li>\
+    </ul>\
+    <ul class="copyright">\
+        <li>&copy; Thu Nguyen. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>\
+    </ul>\
+</div>\
+</section>\
+');
---
+++ assets/js/navbar.js
@@ -0,0 +1,15 @@
+document.write('<nav id="menu">\
+<div class="inner">\
+    <h2>Menu</h2>\
+    <ul class="links">\
+        <li><a href="index.html">Home</a></li>\
+        <li><a href="generic.html">About</a></li>\
+        <li><a href="elements.html">Portfolio</a></li>\
+        <li><a href="service.html">Service</a></li>\
+        <li><a href="ThuNguyen (2).pdf" target="_blank">Resume</a></li>\
+    </ul>\
+    <a href="#" class="close">Close</a>\
+</div>\
+</nav>\
+');
+
