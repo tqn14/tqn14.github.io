b2482d977234b63ea6f5d20704a6f65567ff630a
ThuNguyen <56768188+tqn14@users.noreply.github.com>
2021-09-03 12:46:20 -0500

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -6,7 +6,7 @@ document.write('<nav id="menu">\
         <li><a href="about.html">About</a></li>\
         <li><a href="portfolio.html">Portfolio</a></li>\
         <li><a href="service.html">Service</a></li>\
-        <li><a href="ThuNguyen (4).pdf" target="_blank">Resume</a></li>\
+        <li><a href="resume.pdf" target="_blank">Resume</a></li>\
     </ul>\
     <a href="#" class="close">Close</a>\
 </div>\
