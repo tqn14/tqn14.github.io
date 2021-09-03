39cd5a85bfe5428110241bb57ce462328db5b09a
Thu Nguyen <tqn14@tamu.edu>
2021-09-03 12:19:31 -0500

    edit

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -2,11 +2,11 @@ document.write('<nav id="menu">\
 <div class="inner">\
     <h2>Menu</h2>\
     <ul class="links">\
-        <li><a href="index.html">Home</a></li>\
-        <li><a href="generic.html">About</a></li>\
-        <li><a href="elements.html">Portfolio</a></li>\
+        <li><a href="home.html">Home</a></li>\
+        <li><a href="about.html">About</a></li>\
+        <li><a href="portfolio.html">Portfolio</a></li>\
         <li><a href="service.html">Service</a></li>\
-        <li><a href="ThuNguyen (2).pdf" target="_blank">Resume</a></li>\
+        <li><a href="ThuNguyen (4).pdf" target="_blank">Resume</a></li>\
     </ul>\
     <a href="#" class="close">Close</a>\
 </div>\
